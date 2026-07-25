38f15507c4f4e3ecb96e2285bc0da7ef89e45904
Fatmanur Köroğlu <304889088+fatmanurkoroglu@users.noreply.github.com>
2026-07-25 23:34:15 +0300

    styled the modal bottom sheet a little bit

--- lib/widgets/mood_tracker_screen/add_edit_mood/add_edit_mood.dart
+++ lib/widgets/mood_tracker_screen/add_edit_mood/add_edit_mood.dart
@@ -3,30 +3,59 @@ import 'package:mood_tracker_app_ftmnr/models/mood.dart';
 
 void addEditMoodBottomSheet(BuildContext context) {
   showModalBottomSheet(
+    isScrollControlled: true,
     context: context,
     builder: (BuildContext context) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          //TODO: bu dropdown menuyu stille
-          DropdownButtonFormField(
-            hint: const Text('mood'),
-            items: [],
-            onChanged: (ctx) {},
-          ),
+      return Padding(
+        padding: EdgeInsets.fromLTRB(16, 64, 16, 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DropdownButtonFormField(
+              hint: const Text('How do you feel?'),
+              items: [],
+              onChanged: (ctx) {},
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
 
-          Text('mood note entry'),
+            SizedBox(height: 48),
 
-          Expanded(
-            child: Row(
+            TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('date'),
-                ElevatedButton(onPressed: () {}, child: Text('cancel')),
-                ElevatedButton(onPressed: () {}, child: Text('submit')),
+                Row(
+                  children: [
+                    Text('No date selected'),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.date_range_outlined),
+                    ),
+                  ],
+                ),
+
+                Row(
+                  children: [
+                    TextButton(onPressed: () {}, child: Text('cancel')),
+                    ElevatedButton(onPressed: () {}, child: Text('submit')),
+                  ],
+                ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       );
     },
   );
